43187c715245bfa7a518c3cadca668f19eb11058
haitao <swanduron@hotmail.com>
2020-09-08 22:12:07 +0800

    Change driven mode to Timer based temp rolling.

--- main.py
+++ main.py
@@ -7,11 +7,14 @@ import onewire
 
 DEFAULT_I2C_ADDR = 0x27
 SPEED_VALUE = 249
-TEMP_LOW = 0
-TEMP_HIGH = 50
-SPEED_LOW = 0
+TEMP_LOW = 20
+TEMP_HIGH = 45
+SPEED_LOW = 20
 SPEED_HIGH = 100
 temp = 0
+
+led = Pin(2, Pin.OUT)
+led.value(1)
 
 fan1 = PWM(Pin(5))
 fan2 = PWM(Pin(18))
@@ -21,15 +24,13 @@ fan2.freq(25000)
 fan3.freq(25000)
 fan_matrix = [fan1, fan2, fan3]
 
-
 def fan_operation(fan_list, speed_value):
     for fan in fan_list:
         fan.duty(speed_value)
 
-
 fan_operation(fan_matrix, SPEED_VALUE)
 
-led = Pin(2)
+
 i2c = I2C(scl=Pin(22), sda=Pin(21), freq=400000)
 real_clock = ds3231.DS3231(i2c)
 ds_sensor = ds18x20.DS18X20(onewire.OneWire(Pin(4)))
@@ -41,16 +42,14 @@ lcd.putstr("Temp control\nStart online!")
 
 
 def temp_detect(timer):
-    print('See timer>>:', timer)
     global temp
+    led.value(0 if led.value() else 1)
     ds_sensor.convert_temp()
     sleep_ms(350)
     temp = ds_sensor.read_temp(ds_dev)
 
-
 temp_timer = Timer(1)
 temp_timer.init(period=1000, mode=Timer.PERIODIC, callback=temp_detect)
-
 
 def z_fill(string, position='left', length=4, tag='0'):
 
@@ -58,6 +57,7 @@ def z_fill(string, position='left', length=4, tag='0'):
     if len(string) >= length:
         return string
     else:
+
         if position == 'left':
             for i in range(length - len(string)):
                 string = tag + string
@@ -79,7 +79,8 @@ def get_time():
 
 def speed_change(temp):
 
-    percent = temp / (TEMP_HIGH - TEMP_LOW)
+    percent = (temp - TEMP_LOW) / (TEMP_HIGH - TEMP_LOW)
+    percent = percent if percent > 0 else 0
     speed_diff = ((SPEED_HIGH - SPEED_LOW) * percent + SPEED_LOW) / 100
     fan_operation(fan_matrix, int(1023 * speed_diff))
 
