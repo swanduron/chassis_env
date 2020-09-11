6bd359fe1ddd700321de9500ceb6188c14bc0459
haitao <swanduron@hotmail.com>
2020-09-11 18:14:44 +0800

    Add web server into coding lib.

---
+++ json_engine.py
@@ -0,0 +1,15 @@
+import json
+
+FILE_URL = 'config.json'
+
+data = {
+    'ap': '',
+    'password': '',
+    'temp_low': 0,
+    'temp_high': 100,
+    'speed_low': 20,
+    'speed_high': 95
+}
+
+with open(FILE_URL, mode='w') as file_target:
+    json.dump(data, file_target, indent=2)
